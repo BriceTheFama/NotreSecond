88162a496487a7f87862dd8e504412139d2234e4
BriceTheFama <fglamou@gmail.com>
2021-05-29 06:55:58 +0200

    heroku deply2

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -41,10 +41,10 @@
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/contact">Contact</a>
+                        <a class="nav-link" href="{{ route('contact') }}">Contact</a>
                     </li>
 					<li class="nav-item">
-                        <a class="nav-link" href="/a-propos">A-propos</a>
+                        <a class="nav-link" href="{{ route('about') }}">A-propos</a>
                     </li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/index', function () {
+Route::get('/', function () {
     return view('index');
-});
+})->name('accueil');
 Route::get('/a-propos',function(){
     return view('a-propos');
-});
+})->name('about');
 
-Route::get('/contact',[ContactController::class,'contact']);
-Route::get('/test',function(){ return view('test');});
+Route::get('/contact',[ContactController::class,'contact'])->name('contact');
+Route::get('/test',function(){ return view('test');})->name('test');
 Route::post('/contact',[ContactController::class,'sendEmail'])->name('contact.send');
